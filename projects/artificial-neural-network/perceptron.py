93fb298b4e292b1abc6e604133aedb657de77b10
Sangeetha Sankaran <ssankara@andrew.cmu.edu>
2017-01-06 19:26:04 -0500

    Update perceptron.py

--- projects/artificial-neural-network/perceptron.py
+++ projects/artificial-neural-network/perceptron.py
@@ -38,3 +38,16 @@ class Perceptron:
         
         # Then return 0 or 1 depending on strength compared to threshold  
         return int(strength > self.threshold)
+    
+    def test():
+    """
+    A few tests to make sure that the perceptron class performs as expected.
+    Nothing should show up in the output if all the assertions pass.
+    """
+    p1 = Perceptron(np.array([1, 2]), 0.)
+    assert p1.activate(np.array([ 1,-1])) == 0 # < threshold --> 0
+    assert p1.activate(np.array([-1, 1])) == 1 # > threshold --> 1
+    assert p1.activate(np.array([ 2,-1])) == 0 # on threshold --> 0
+
+    if __name__ == "__main__":
+    test()
