33380c7429245a388913df601325ff03e1e286ea
Sangeetha Sankaran <ssankara@andrew.cmu.edu>
2017-01-06 19:23:47 -0500

    Create perceptron.py

---
+++ projects/artificial-neural-network/perceptron.py
@@ -0,0 +1,40 @@
+# ----------
+#
+# In this exercise, you will update the perceptron class so that it can update
+# its weights.
+#
+# Finish writing the update() method so that it updates the weights according
+# to the perceptron update rule. Updates should be performed online, revising
+# the weights after each data point.
+# 
+# ----------
+
+import numpy as np
+
+
+class Perceptron:
+    """
+    This class models an artificial neuron with step activation function.
+    """
+
+    def __init__(self, weights = np.array([1]), threshold = 0):
+        """
+        Initialize weights and threshold based on input arguments. Note that no
+        type-checking is being performed here for simplicity.
+        """
+        self.weights = weights.astype(float) 
+        self.threshold = threshold
+
+
+    def activate(self, values):
+        """
+        Takes in @param values, a list of numbers equal to length of weights.
+        @return the output of a threshold perceptron with given inputs based on
+        perceptron weights and threshold.
+        """
+               
+        # First calculate the strength with which the perceptron fires
+        strength = np.dot(values,self.weights)
+        
+        # Then return 0 or 1 depending on strength compared to threshold  
+        return int(strength > self.threshold)
